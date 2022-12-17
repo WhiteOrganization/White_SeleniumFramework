7335ef0e64d2e843ecfffa8461cd5ad1de58e72a
Obed Vazquez <obed.vazquez@gmail.com>
2022-12-17 17:07:10 -0600

    - Introduced Table data extraction support - Various method corrections - Upgraded version due to breaking compatibility in older patches - Preparing for version 0.3.0

--- README.md
+++ README.md
@@ -33,7 +33,7 @@ WebDriver webDriver=util.driver;
 The framework will also take care of the entire configuration of the Drivers, you just need to specify what supported WebExplorers do you want to run your scenarios on.
 
 
-* Version: [0.2.9](https://github.com/orgs/WhiteOrganization/packages) 
+* Version: [0.3.0](https://github.com/orgs/WhiteOrganization/packages) 
 
     This is still in development and some methods are being constantly added as they are used. Please help us by requesting those you need or need more detailed documentation.
 
@@ -48,7 +48,7 @@ by including it in your POM](https://maven.pkg.github.com/whiteorganization/whit
 	<dependency>
     		<groupId>io.github.whiteorganization</groupId>
     		<artifactId>white-selenium-framework</artifactId>
-    		<version>0.2.9</version>
+    		<version>0.3.0</version>
 	</dependency>
 ```
 If used on a long-term heavy-use project we recommend:
@@ -61,7 +61,7 @@ If used on a long-term heavy-use project we recommend:
         <dependency>
             <groupId>io.github.whiteorganization</groupId>
             <artifactId>white-selenium-framework</artifactId>
-            <version>0.2.9</version>
+            <version>0.3.0</version>
             <!-- You need to manually import this library due to lack of maintenance on White_SeleniumFramework -->
             <exclusions>
                 <exclusion>
@@ -101,7 +101,7 @@ and import the dependency like this:
 	<dependency>
 	    <groupId>com.github.WhiteOrganization</groupId>
 	    <artifactId>White_SeleniumFramework</artifactId>
-	    <version>white-selenium-framework-0.2.9</version>
+	    <version>white-selenium-framework-0.3.0</version>
 	</dependency>
 ```
 
@@ -145,8 +145,6 @@ You can just copy and paste this into your project or edit it
 this library uses
 - lombok to log errors and general logs.
 	- Slf4j
-- PropertiesManager to obtain the default properties values.
-- White_Validations to validate parameters.
 
 * Database configuration
   None
@@ -154,16 +152,17 @@ this library uses
   No need for those. Some working projects using this library are:
 - [White_LoLPicker](https://github.com/white-sdev/White_LoLPicker)
 
-## How to Deploy?
+## Contributions
+### How to Deploy?
 use [maven](https://spring.io/guides/gs/maven/) to release new version to any Dependency Repository.
 
-### GitHub and JitPack
+#### GitHub and JitPack
 You usually don't need to execute manually a release, configured GitHub actions will take care of the Build and 
 release process for SNAPSHOT's but in case of a version you can do
 
 	mvn release:clean release:prepare -D target=github
 
-### Sonatype - Nexus - Maven Central Repository (Configuring)
+#### Sonatype - Nexus - Maven Central Repository
 for SNAPSHOT's use:
 
 	mvn clean deploy -D target=sonatype
@@ -176,14 +175,16 @@ Followed by:
 	mvn clean deploy -D target=sonatype
 	mvn release:perform -D target=sonatype
 
-## Contribution guidelines ###
+### Contribution guidelines ###
 
 * Writing tests.
 
-Please do. Only some basic Unit testing is set up at the moment
-* Code review.
+Please do. Only some basic Unit testing is set up at the moment.
 
-Request if needed
+Request if you need more specification
+* Code review.
+ 
+Forced for pull requests
 * Other guidelines.
 
 Ask for the code standard to use as guidelines and reflect them in here.
--- src/test/java/org/white_sdev/white_seleniumframework/framework/prodexcluded/WebDriverUtilsTest.java
+++ src/test/java/org/white_sdev/white_seleniumframework/framework/prodexcluded/WebDriverUtilsTest.java
@@ -3,29 +3,31 @@ package org.white_sdev.white_seleniumframework.framework.prodexcluded;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.openqa.selenium.WebDriver;
 import org.white_sdev.white_seleniumframework.framework.AutomationScenario;
 import org.white_sdev.white_seleniumframework.framework.AutomationSuite;
 import org.white_sdev.white_seleniumframework.framework.WebDriverUtils;
-import org.white_sdev.white_seleniumframework.utils.PropertiesReader;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.white_sdev.white_seleniumframework.utils.PropertiesReader.setProperty;
 
 public class WebDriverUtilsTest {
 	
 	@BeforeAll
 	public static void setUp() {
-		setProperty("white-selenium-framework.execute.web-driver.chrome", "false");
+		setProperty("white-selenium-framework.execute.web-driver.chrome", "true");
 		setProperty("white-selenium-framework.execute.web-driver.ie", "false");
 		setProperty("white-selenium-framework.execute.web-driver.edge", "false");
 		setProperty("white-selenium-framework.execute.web-driver.firefox", "false");
-		setProperty("white-selenium-framework.execute.web-driver.browserless", "true");
+		setProperty("white-selenium-framework.execute.web-driver.browserless", "false");
 		setProperty("white-selenium-framework.close-on-error", "true");
 	}
 	
@@ -46,6 +48,13 @@ public class WebDriverUtilsTest {
 	@Test
 	public void tripleQuittingTest() {
 		AutomationSuite.registerAutomationScenario(new DoubleQuitting());
+		AutomationSuite.launchExecutions();
+	}
+	
+	@Test
+	@Disabled
+	public void simpleTableExtractionTest() {
+		AutomationSuite.registerAutomationScenario(new TableExtraction());
 		AutomationSuite.launchExecutions();
 	}
 	
@@ -88,9 +97,41 @@ public class WebDriverUtilsTest {
 	public static class DoubleQuitting implements AutomationScenario {
 		
 		@Override
-		public void run(final WebDriverUtils utils){
-			utils.quit();
-			utils.quit();
+		public void run(final WebDriverUtils utils) {
+			utils.closeWebBrowserWindow();
+			utils.closeWebBrowserWindow();
+		}
+		
+		@Override
+		public String getScenarioFullName() {
+			return getClass().getCanonicalName();
+		}
+	}
+	
+	@Slf4j
+	public static class TableExtraction implements AutomationScenario {
+		
+		@Override
+		public void run(final WebDriverUtils utils) {
+			String logID = "::run([utils]): ";
+			log.trace("{}Start ", logID);
+			utils.openURL("https://www.techlistic.com/p/demo-selenium-practice.html");
+			Optional<List<LinkedHashMap<String, String>>> table = utils.getTableDataWithId("customers");
+			assertThat(table).isNotEmpty();
+			
+			log.info("{}{}", logID, table.get());
+			List<LinkedHashMap<String, String>> filtered = new ArrayList<>();
+			for (LinkedHashMap<String, String> row : table.get())
+				if (row.containsKey("Company") && row.get("Company").equals("Google"))
+					filtered.add(row);
+
+//			List<LinkedHashMap<String, String>> filtered = table.get()
+//					.stream()
+//					.filter((LinkedHashMap<String,String> map)-> map.get("Company").equals("Google"))
+//						.collect(Collectors.toList());
+			assertThat(filtered).isNotEmpty();
+//			utils.closeWebBrowserWindow();
+			
 		}
 		
 		@Override
